783511bffe5068e255fe9586f68709dbaaec8227
FurkanCsk <furkancoskun99@hotmail.com>
2024-10-18 20:38:24 +0300

    Add project files.

---
+++ Practice-Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Practice_Identity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ Practice-Identity/ViewModel/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Practice_Identity.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
